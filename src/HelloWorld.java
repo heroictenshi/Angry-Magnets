000670cc53dce4ab5bd36549e3929274253a3938
Jeffry Lien <heroictenshi@users.noreply.github.com>
2014-05-11 15:19:35 -0400

    src code add

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Jeffry Lien on 11/05/2014.
+ */
+public class HelloWorld
+{
+    public void printText ()
+    {
+        System.out.println("Cake");
+    }
+
+
+    public static void main (String [] args)
+    {
+        HelloWorld cake = new HelloWorld();
+        cake.printText();
+    }
+}
